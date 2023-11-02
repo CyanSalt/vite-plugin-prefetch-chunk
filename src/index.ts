4844250469f0a9ae236e6f15e1f245b5a8cb8d3a
CyanSalt <c.salt@hotmail.com>
2023-11-02 20:53:54 +0800

    fix: asset path in html

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,80 @@
-import type { Plugin } from 'vite'
+import * as path from 'path'
+import type { Plugin, ResolvedConfig, Rollup } from 'vite'
+import { normalizePath } from 'vite'
+
+// Duplicated from build.ts in Vite Core, at least while the feature is experimental
+// We should later expose this helper for other plugins to use
+function toOutputFilePathInHtml({
+  filename,
+  type,
+  hostId,
+  hostType,
+  config,
+  toRelative,
+}: {
+  filename: string,
+  type: 'asset' | 'public',
+  hostId: string,
+  hostType: 'js' | 'css' | 'html',
+  config: ResolvedConfig,
+  toRelative: (filename: string, importer: string) => string,
+}): string {
+  const { renderBuiltUrl } = config.experimental
+  let relative = config.base === '' || config.base === './'
+  if (renderBuiltUrl) {
+    const result = renderBuiltUrl(filename, {
+      hostId,
+      hostType,
+      type,
+      ssr: Boolean(config.build.ssr),
+    })
+    if (typeof result === 'object') {
+      if (result.runtime) {
+        throw new Error(
+          `{ runtime: "${result.runtime}" } is not supported for assets in ${hostType} files: ${filename}`,
+        )
+      }
+      if (typeof result.relative === 'boolean') {
+        relative = result.relative
+      }
+    } else if (result) {
+      return result
+    }
+  }
+  if (relative && !config.build.ssr) {
+    return toRelative(filename, hostId)
+  } else {
+    return config.base + filename
+  }
+}
+function getBaseInHTML(urlRelativePath: string, config: ResolvedConfig) {
+  // Prefer explicit URL if defined for linking to assets and public files from HTML,
+  // even when base relative is specified
+  return config.base === './' || config.base === ''
+    ? path.posix.join(
+      path.posix.relative(urlRelativePath, '').slice(0, -2),
+      './',
+    )
+    : config.base
+}
+
+function toAssetPathFromHtml(
+  filename: string,
+  htmlPath: string,
+  config: ResolvedConfig,
+): string {
+  const relativeUrlPath = normalizePath(path.relative(config.root, htmlPath))
+  const toRelative = (filePath: string, hostId: string) =>
+    getBaseInHTML(relativeUrlPath, config) + filePath
+  return toOutputFilePathInHtml({
+    filename,
+    type: 'asset',
+    hostId: htmlPath,
+    hostType: 'html',
+    config,
+    toRelative,
+  })
+}
 
 const requestQuerySplitRE = /\?(?!.*[/|}])/
 
@@ -15,6 +91,7 @@ export interface Options {
 
 const prefetchChunk = (options: Options = {}): Plugin => {
   const prefetchModules = new Set<string>()
+  let config: ResolvedConfig
   return {
     name: 'vite-plugin-prefetch-chunk',
     resolveId: {
@@ -32,28 +109,35 @@ const prefetchChunk = (options: Options = {}): Plugin => {
         }
       },
     },
+    configResolved(resolvedConfig) {
+      config = resolvedConfig
+    },
     load(id) {
       if (id === '\0virtual:prefetch') {
         return ''
       }
     },
-    transformIndexHtml(html, { bundle }) {
+    transformIndexHtml(html, { filename: htmlPath, bundle }) {
       if (!bundle) return html
-      const chunks = Object.values(bundle).filter(chunk => {
-        if (chunk.type === 'chunk' && chunk.isEntry) return false
+      const chunks = Object.values(bundle).filter((chunk): chunk is Rollup.OutputChunk => {
+        if (chunk.type !== 'chunk' || chunk.isEntry) return false
         if (!options.prefetchLegacyChunks && chunk.fileName.includes('-legacy')) return false
         return true
       })
       const prefetchFiles = new Set<{ href: string, as?: string }>()
       for (const chunk of chunks) {
-        const renderedModules = chunk.type === 'chunk' ? chunk.modules : {}
-        for (const id of Object.keys(renderedModules)) {
+        for (const id of Object.keys(chunk.modules)) {
           if (prefetchModules.has(id)) {
-            prefetchFiles.add({ href: chunk.fileName })
+            prefetchFiles.add({
+              href: toAssetPathFromHtml(chunk.fileName, htmlPath, config),
+            })
             const importedCss: Set<string> | undefined = chunk['viteMetadata']?.importedCss
             if (importedCss) {
-              for (const url of importedCss) {
-                prefetchFiles.add({ href: url, as: 'style' })
+              for (const file of importedCss) {
+                prefetchFiles.add({
+                  href: toAssetPathFromHtml(file, htmlPath, config),
+                  as: 'style',
+                })
               }
             }
           }
